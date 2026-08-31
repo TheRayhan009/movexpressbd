3f97a2a4a52b23e73cc0a0472d37c880810198a2
TheRayhan009 <rayhanthestorymaker@gmail.com>
2026-08-31 21:58:51 +0600

    second

--- movexpressd/settings.py
+++ movexpressd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u+z266aqx7zorymkri_8677#^)bph4#hd&qz$@$wui*29)!!y+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

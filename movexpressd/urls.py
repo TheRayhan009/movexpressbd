32c7ac625fff4ee2a74dd938579efc3f276945d8
TheRayhan009 <rayhanthestorymaker@gmail.com>
2026-09-01 00:06:34 +0600

    sidemap

--- movexpressd/urls.py
+++ movexpressd/urls.py
@@ -1,28 +1,24 @@
-"""
-URL configuration for movexpressd project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.contrib.sitemaps.views import sitemap
 from main import views
+from main.sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home,name="home"),
-    path('order/', views.order,name="order"),
-    path('founderrayhan/', views.founderrayhan,name="founderrayhan"),
-    path('founderarafat/', views.founderarafat,name="founderarafat"),
+
+    path('', views.home, name="home"),
+    path('order/', views.order, name="order"),
+    path('founderrayhan/', views.founderrayhan, name="founderrayhan"),
+    path('founderarafat/', views.founderarafat, name="founderarafat"),
+
+    # Sitemap
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
